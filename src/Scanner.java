0e9cb98351713e73fae9d6c85da0d616e983f8a0
Otavio H. P. Valadares <otaviopvaladares@gmail.com>
2020-06-24 02:43:04 -0300

    Lex: String

--- src/Scanner.java
+++ src/Scanner.java
@@ -67,6 +67,26 @@ class Scanner {
         }
     }
 
+    private void string() {
+        while (peek() != '"' && !isAtEnd()) {
+            if (peek() == '\n') line++;
+            advance();
+        }
+
+        // Unterminated string.
+        if (isAtEnd()) {
+            Lox.error(line, "Unterminated string.");
+            return;
+        }
+
+        // The closing ".
+        advance();
+
+        // Trim the surrounding quotes.
+        String value = source.substring(start + 1, current - 1);
+        addToken(TokenType.STRING, value);
+    }
+
     private boolean match(char expected) {
         if (isAtEnd()) return false;
         if (source.charAt(current) != expected) return false;
